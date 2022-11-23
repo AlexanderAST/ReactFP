0da783621092e4ef5e55ac2b83ea10d025b8d10a
AlexanderAST <89164909109@bk.ru>
2022-11-23 11:00:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import "./style/App.css";
+import PostItem from "./components/PostItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts,setPost]=useState([
+        {id: 1,title:"Golang",body:"Description"},
+        {id: 2,title:"Golang 2",body:"Description"},
+        {id: 3,title:"Golang 3",body:"Description"},
+
+    ])
+
+
+    return (
+        <div className="App">
+            <Counter></Counter>
+            <ClassCounter></ClassCounter>
+
+            {posts.map(post=>
+               <PostItem post={post} key={post.id} />
+            )}
+
+
+        </div>
+
+    );
+
 }
 
 export default App;
